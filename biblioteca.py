bd952276f687c0a039137d7cce1f501fedd6e12f
João Pedro Almeida Menino <joaopedroalmeidamenino@gmail.com>
2024-06-27 12:38:40 -0300

    criando lista com split

--- biblioteca.py
+++ biblioteca.py
@@ -2,17 +2,26 @@ from tinydb import TinyDB
 from dataclasses import dataclass, asdict
 from pathlib import Path
 
+
 @dataclass
-class todo:
+class livro:
     nome_livro: str
     quantidade: int
 
     def dict(self):
         return asdict(self)
 
+    def teste():
+        livros = input(f"Digite os nomes dos livro separados por virgula: ").replace(' ','')
+        livro_lista = livros.split(',')
+        quantidades = input(f'Digite a quantiddes dos livro separados por virgula: ').replace(' ','')
+        quantidades_lista = quantidades.split(',')
 
-t1 = todo('ojh','4')
+        print(livro_lista, quantidades_lista)
+    teste()
+    
 
+t1 = livro('ojh','4')
 
 
 db_path = Path(__file__).parent /'biblioteca.json'
